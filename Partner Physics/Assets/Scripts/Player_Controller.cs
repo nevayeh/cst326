dcff1abda2b83884e93a9435ea1c97a14837c646
nevayeh <nyeh@csumb.edu>
2019-02-11 21:08:12 -0800

    Added Partner Physics project

---
+++ Partner Physics/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+    private Rigidbody rb;
+    public Camera cameraRefrence;
+
+    public float speed;
+    public float jumpHeight;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(cameraRefrence.transform.up * jumpHeight, ForceMode.Impulse);
+        }
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Partner Physics/Assets/Scripts/YouWin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour {
+    public Text youWinText;
+    public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        youWinText.text = "";
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        youWinText.text = "You Win";
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 origin = new Vector3(-10, -10, 0);
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(origin, mainCamera.transform.right,  out hit))
+        {
+            youWinText.text = "You Lose";
+        }
+    }
+}
