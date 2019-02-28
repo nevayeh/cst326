27589766fd8e356a0802d1b475b779c444dcd931
nevayeh <nyeh@csumb.edu>
2019-02-28 12:02:10 -0800

    added Arkanoid project (scope testing lab)

---
+++ Arkanoid/Assets/Scripts/BallScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+    private Vector2 ballResetForce;
+
+    // GameObject
+    public GameObject playerObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        // create the force
+        ballInitialForce = new Vector2(100.0f, 200.0f);
+
+        ballResetForce = new Vector2(0.0f, 0.0f);
+
+        // set to inactive
+        ballIsActive = false;
+
+        // ball position
+        ballPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // check for user input
+        if (Input.GetButtonDown("Jump") == true)
+        {
+            // check if is the first play
+            if (!ballIsActive)
+            {
+                // set ball active
+                ballIsActive = !ballIsActive;
+
+                // add a force
+                rigidbody2D.AddForce(ballInitialForce);
+            }
+        }
+
+        if (!ballIsActive && playerObject != null)
+        {
+            // get and use the player position
+            ballPosition.x = playerObject.transform.position.x;
+
+            // apply player X position to the ball
+            transform.position = ballPosition;
+        }
+
+        // Check if ball falls
+        if (ballIsActive && transform.position.y < -6)
+        {
+            ballIsActive = !ballIsActive;
+            ballPosition.x = playerObject.transform.position.x;
+            ballPosition.y = -3.2f;
+            transform.position = ballPosition;
+
+            rigidbody2D.velocity = ballResetForce;
+
+            playerObject.SendMessage("TakeLife");
+        }
+
+    }
+}
---
+++ Arkanoid/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float playerVelocity;
+    private Vector3 playerPosition;
+    public float boundary;
+    private int playerLives;
+    private int playerPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        // get the initial position of the game object
+        playerPosition = gameObject.transform.position;
+
+        playerLives = 0;
+        playerPoints = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // horizontal movement
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity;
+
+        // leave the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // update the game object transform
+        transform.position = playerPosition;
+
+        // boundaries
+        if (playerPosition.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, playerPosition.y, playerPosition.z);
+        }
+        if (playerPosition.x > boundary)
+        {
+            transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);
+        }
+
+    }
+
+    void addPoints(int points)
+    {
+        playerPoints += points;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(5.0f, 3.0f, 200.0f, 200.0f), "Deaths: " + playerLives + "  Score: " + playerPoints);
+    }
+
+    void TakeLife()
+    {
+        playerLives++;
+    }
+}
