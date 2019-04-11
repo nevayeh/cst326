670251d649ea63e92aac75555c31fbfb937a33da
nevayeh <42818611+nevayeh@users.noreply.github.com>
2019-04-10 17:49:33 -0700

    Added Special Topics Tutorials Lab

---
+++ Special Topics Tutorials/Assets/Script/NPCMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    public Transform target;
+    public Transform point1;
+    public Transform point2;
+    private bool chase;
+    public int minDistance;
+    UnityEngine.AI.NavMeshAgent agent;
+    //NavMeshAgent somethingElse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //somethingElse = this.GetComponent<NavMeshAgent>();
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        chase = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target.transform.position) < minDistance)
+        {
+            SetDestination();
+        }
+        else
+        {
+            SetPatrol();
+        }
+        
+    }
+
+    private void SetDestination()
+    {
+        if(target != null)
+        {
+            Vector3 targetVector = target.transform.position;
+            //somethingElse.SetDestination(targetVector);
+            agent.SetDestination(targetVector);
+            chase = true;
+        }
+    }
+
+    private void SetPatrol()
+    {
+        if (chase == false)
+        {
+            if (Vector3.Distance(transform.position, point1.transform.position) < 0.5 && Vector3.Distance(transform.position, point2.transform.position) > 1.0)
+            {
+                Vector3 patrolVector = point2.transform.position;
+                agent.SetDestination(patrolVector);
+            }
+            else if (Vector3.Distance(transform.position, point1.transform.position) > 1 && Vector3.Distance(transform.position, point2.transform.position) < 0.5)
+            {
+                Vector3 patrolVector = point1.transform.position;
+                agent.SetDestination(patrolVector);
+            }
+        }
+        else
+        {
+            if(Vector3.Distance(transform.position, point1.transform.position) < Vector3.Distance(transform.position, point2.transform.position))
+            {
+                Vector3 patrolVector = point2.transform.position;
+                agent.SetDestination(patrolVector);
+                chase = false;
+            }
+            else
+            {
+                Vector3 patrolVector = point1.transform.position;
+                agent.SetDestination(patrolVector);
+                chase = false;
+            }
+        }
+    }
+}
---
+++ Special Topics Tutorials/Assets/Script/PlayerControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed;
+    public GameObject camera;
+    private Rigidbody rb;
+
+    public Transform player;
+    public float turnSpeed = 4.0f;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        offset = new Vector3(player.position.x, player.position.y + 3.0f, player.position.z - 8.0f);
+    }
+        
+    void FixedUpdate()
+    {
+        Vector3 cameraPlayerDifference = transform.position - camera.transform.position;
+        cameraPlayerDifference.y = 0;
+        cameraPlayerDifference.Normalize();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = (cameraPlayerDifference * moveVertical + camera.transform.right * moveHorizontal) * speed;
+        Vector3 up = new Vector3(0.0f, jump, 0.0f);
+
+        rb.AddForce(movement * speed);
+        if(Input.GetKeyDown("space"))
+        {
+            rb.AddForce(up * 600);
+        }
+    }
+}
